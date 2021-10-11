6b6cb63aed4a024c4575d794706acccd5d006cfa
JP <balaji9balaji9@gmail.com>
2021-10-11 19:43:02 +0200

    Included rsync cmd for harddisk redundancy

--- PlexAutomation/manualRestartAll.sh
+++ PlexAutomation/manualRestartAll.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sudo umount /mnt/plexmedia/Media;
-sudo umount /mnt/plexmedia;
+sudo umount /media/PlexMediaFiles/Media;
+sudo umount /media/PlexMediaFiles;
+sudo umount /media/PlexMediaFiles_Backup;
 sleep 10;
-sudo mount /dev/sdb1 /mnt/plexmedia;
+sudo mount /dev/sda1 /media/PlexMediaFiles;
+sudo mount /dev/sdb1 /media/PlexMediaFiles_Backup;
 sudo service plexmediaserver restart;
 
 export LD_LIBRARY_PATH="/usr/lib/plexmediaserver/lib";
-sudo su -s /bin/bash plex -c "/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --force";
+sudo su -s /bin/bash plex -c "/usr/lib/plexmediaserver/Plex\ Media\ Scanner --scan --refresh --force"
 #su plex /home/pi/Desktop/PlexAutomation/scan.sh
 #sudo killall -9 syncLibraries.sh; /home/pi/Desktop/PlexAutomation/syncLibraries.sh;
