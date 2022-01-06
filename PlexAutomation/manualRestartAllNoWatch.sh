59ba65bb2582011a8685bcead5c5329273cdd2af
JP <balaji9balaji9@gmail.com>
2022-01-06 16:49:51 +0100

    Update mnt location

--- PlexAutomation/manualRestartAllNoWatch.sh
+++ PlexAutomation/manualRestartAllNoWatch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo umount /media/PlexMediaFiles/Media;
-sudo umount /media/PlexMediaFiles;
-sudo umount /media/PlexMediaFiles_Backup;
+sudo umount /media/pi/PlexMediaFiles;
+sudo umount /media/pi/PlexMediaFiles_Backup;
 sleep 10;
-sudo mount /dev/sda1 /media/PlexMediaFiles;
-sudo mount /dev/sdb1 /media/PlexMediaFiles_Backup;
+sudo mount /dev/sda1 /media/pi/PlexMediaFiles;
+sudo mount /dev/sdb1 /media/pi/PlexMediaFiles_Backup;
 sudo service plexmediaserver restart
